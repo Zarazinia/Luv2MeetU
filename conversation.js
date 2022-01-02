f1aaa55127ddafea4a6d89946b4bbc2330ad401f
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2022-01-02 10:39:38 +0000

    Update conversation.js

--- conversation.js
+++ conversation.js
@@ -1,8 +1,14 @@
 var quotes = [
  
 
-    'conversation 1?',
-    'conversation 2?'
+    'What do you think about cheese?',
+    'Do you enjoy cooking? What do like to cook?',
+    'Do you like going to new places?',
+    'Where are your favourite places to visit?',
+    'Would you try new foods?',
+    'Would you try new places?',
+    'Would you try new hobbies?',
+    'Are you interested in politics?'
 
 ]
 
