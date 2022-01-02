316c436578520f62d40511495dbd66e49e225e41
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2022-01-02 10:34:07 +0000

    Update dating.js

--- dating.js
+++ dating.js
@@ -1,8 +1,11 @@
 var quotes = [
  
 
-    'line 1?',
-    'line2?'
+    'What is your perfect date?',
+    'Are you looking for a long term relationship?',
+    'Do you like speaking on the telephone or texting?',
+    'Is it important to you to be friends first?',
+    'Do you have hobbies?'
 
 ]
 
