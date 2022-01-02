66e92154d4a07b3d47149729fa25ad536903b51d
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2022-01-02 11:30:23 +0000

    Update dating.js

--- dating.js
+++ dating.js
@@ -5,6 +5,15 @@ var quotes = [
     'Are you looking for a long term relationship?',
     'Do you like speaking on the telephone or texting?',
     'Is it important to you to be friends first?',
+    'What kind of music do you listen to?',
+    'Have you been anywhere interesting lately?',
+    'Do you have any hidden talents?',
+    'Do you enjoy cooking? What do like to cook?',
+    'Do you like going to new places?',
+    'Where are your favourite places to visit?',
+    'Would you try new foods?',
+    'Would you try new places?',
+    'Would you try new hobbies?',
     'Do you have hobbies?'
 
 ]
