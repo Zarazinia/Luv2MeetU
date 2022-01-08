28d48b7a070ff358c852b0838ef2f412251f27e5
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2022-01-08 17:05:15 +0000

    Update dating.js

--- dating.js
+++ dating.js
@@ -1,6 +1,14 @@
-var quotes = [
+var quotes = [           
  
-
+    'What/’s your favourite thing to do at the weekend?',
+    'Do you like being out with a group of people or do you prefer just one or two people at a time?',
+    'What are the three most important things that make you happy?',
+    'Do you have a job or volunteer?',
+    'What would you say are the best things about you, being kind, helping people or able to make people laugh maybe?',
+    'Do you have any bad habits?',
+    'Are you looking for a long term relationship, how would you like it to go?',
+    'Can you give me some examples of music that you like and when you listen to it?',
+    'Are you interested in any subjects like history, art or space? Can you tell me about them?',
     'What is your perfect date?',
     'Are you looking for a long term relationship?',
     'Do you like speaking on the telephone or texting?',
@@ -9,7 +17,7 @@ var quotes = [
     'Have you been anywhere interesting lately?',
     'Do you have any hidden talents?',
     'Do you enjoy cooking? What do like to cook?',
-    'Do you like going to new places?',
+    'What activities or places do you enjoy going to? Bowling, cinema or walking in the countryside maybe?',
     'Where are your favourite places to visit?',
     'Would you try new foods?',
     'Would you try new places?',
