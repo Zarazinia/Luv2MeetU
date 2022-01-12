a1233117b583f331542035363b632da13cc04a79
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2022-01-12 10:34:55 +0000

    Update inspire.js

--- inspire.js
+++ inspire.js
@@ -1,6 +1,20 @@
 var inspire = [
  
-
+    'Keep calm',
+    'Take deep breaths!',
+    'Be Brave!',
+    'Keep trying',
+    'Love Life!',
+    'Be Awesome!',
+    'Stay strong!',
+    'Be Kind',
+    'Yes you can!',
+    'Go for it!',
+    'Stay positive',
+    'Never give up!',
+    'Just be you',
+    'Be proud of you',
+    'Make some-one smile!',
     'Be happy!',
     'Don\'t panic!', 
     'Smile!'
